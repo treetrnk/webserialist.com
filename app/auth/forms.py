1f902f2325d4cf5721b66e69efef249e5df40672
Nathan Hare <nhare330@gmail.com>
2019-09-12 11:31:17 -0400

    Begin work on signup form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,7 +6,19 @@ from wtforms import (
 )
 from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
 from wtforms.validators import DataRequired, Length, Email, Optional, EqualTo, ValidationError
+from app.models import User
 
 required = "<span class='text-danger'>*</span>"
 
-# Add forms here
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(max=64)])
+    first_name = StringField('First Name' validators=[Length(max=100)])
+    last_name = StringField('Last Name', validators=[Length(max=100)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(max=128)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password'), Length(max=128)])
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=self.username.data).first()
+        if user is not None:
+            raise ValidationError('Username already in use.', 'danger')
+        return True
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,5 +2,18 @@ from flask import render_template, redirect, flash, url_for
 from app import db
 from app.auth import bp
 from flask_login import login_required, current_user
+from app.auth.forms import SignupForm
 
 # Add routes here
+@bp.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User()
+        form.populate_obj(user)
+        user.set_password(form.password.data)
+        flash('Welcome to Web Serialist.com!', 'success')
+        return redirect(request.url)
+    return render_template('auth/signup.html',
+            title = 'Sign Up',
+        )
