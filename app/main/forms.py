073b168e8e04e8cd4b72fffc89673154bd6fc50f
Nathan Hare <nhare330@gmail.com>
2019-09-13 11:36:55 -0400

    Build add fiction form

--- app/main/forms.py
+++ app/main/forms.py
@@ -10,3 +10,14 @@ from wtforms.validators import DataRequired, Length, Email, Optional, EqualTo, V
 required = "<span class='text-danger'>*</span>"
 
 # Add forms here
+class AddFictionForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(max=150)])
+    subtitle = StringField('Subtitle', validators=[Length(max=150)])
+    synopsis = StringField('Synopsis', validators=[DataRequired(), Length(max=400)])
+    cover_img = FileField('Cover Image')
+    generes = QuerySelectMultipleField('Genres')
+    website = StringField('URL', validators=[DataRequired(), Length(max=300)])
+    author_claim = BooleanField('Are you the Author?')
+    author_placeholder = StringField('Author', validators=[DataRequried()])
+    status = SelectField('Status')
+    frequency = FloatField('Releases per Month')
