5041b3e2f317473e14b3a296baef156214a262a1
Nathan Hare <nhare330@gmail.com>
2020-03-14 19:56:43 -0400

    Add very basic subscriber list page

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -2,5 +2,15 @@ from flask import render_template, redirect, flash, url_for
 from app import db
 from app.admin import bp
 from flask_login import login_required, current_user
+from app.models import Subscriber
 
 # Add routes here
+@bp.route('/admin/subscribers')
+@login_required
+def subscribers():
+    if current_user.is_authenticated and current_user.username == 'hueyhare' and current_user.id == 1:
+        subscribers = Subscriber.query.all()
+        return render_template('admin/subscribers.html',
+                subscribers=subscribers,
+            )
+    return redirect(url_for('main.index')), 404
